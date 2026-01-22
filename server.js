8ceefe7eb678bf6830dd8af132ac70aa45b9be27
maugos1813 <maufabagosgam@gmail.com>
2026-01-22 21:52:33 +0100

    commit de handle errors

--- server.js
+++ server.js
@@ -1,23 +1,51 @@
 const express = require("express");
 const expressLayouts = require("express-ejs-layouts");
 const dotenv = require("dotenv").config();
+const inventoryRoute = require("./routes/inventoryRoute");
+const baseController = require("./controllers/baseController");
 const static = require("./routes/static");
+const utilities = require("./utilities");
 
 const app = express();
 
+// Servir toda la carpeta public
+app.use(express.static("public"));
+
+// Configuración de EJS
 app.set("view engine", "ejs");
 app.use(expressLayouts);
 app.set("layout", "./layouts/layout");
 
+// Middleware estático propio
 app.use(static);
 
-app.get("/", (req, res) => {
-  res.render("index", { title: "CSE Motors Home" });
+// Ruta principal
+// Index route
+app.get("/", utilities.handleErrors(baseController.buildHome));
+
+
+// Rutas de inventario
+app.use("/inv", inventoryRoute);
+
+// Middleware 404 - para rutas no encontradas
+app.use(async (req, res, next) => {
+  next({ status: 404, message: "Sorry, we appear to have lost that page." });
 });
 
+// Express Error Handler - debe ir después de todo lo demás
+app.use(async (err, req, res, next) => {
+  let nav = await utilities.getNav();
+  console.error(`Error at: "${req.originalUrl}": ${err.message}`);
+  res.status(err.status || 500).render("errors/error", {
+    title: err.status || "Server Error",
+    message: err.message,
+    nav,
+  });
+});
+
+// Servidor
 const port = process.env.PORT || 5500;
 const host = process.env.HOST || "localhost";
-
 app.listen(port, () => {
   console.log(`app listening on ${host}:${port}`);
 });
